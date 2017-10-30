d018c79002d37d45268cf88986e96661260e7aff
phongnx <phongnx@runsystem.net>
2017-10-30 18:27:06 +0900

    temp

--- app/storeConf.js
+++ app/storeConf.js
@@ -7,6 +7,7 @@ function getList() {
   var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6IjU5ZWYxM2E0YjMwOTIifQ.eyJpc3MiOiJodHRwczpcL1wvc3RnLWlkLmdtby5qcFwvYXBpXC92MVwvYXV0aFwvdG9rZW4iLCJhdWQiOiJHTU9JRCIsImp0aSI6IjU5ZWYxM2E0YjMwOTIiLCJpYXQiOjE1MDg4NDAzNTYsImV4cCI6MTUwOTAxMzE1Niwic2l0ZV9pZCI6Ijk5OTk5IiwiZW5jb2RlX3R5cGUiOjB9.QuhEhl3qNuBf6nlf0j2x41TxcZJp2cURLZqDrOAz9EU';
   var myHeaders = new Headers();
   var data = {"login":"gmoidtest0001@gmail.com"};
+
   myHeaders.append('Content-Type', 'application/json');
   myHeaders.append('gmo-id-point-api-token', token);
 
@@ -16,19 +17,36 @@ function getList() {
     body: JSON.stringify(data),
   };
 
-  var request = new Request('https://stg-id.gmo.jp/api/v1/member/get-uid', options);
-  fetch(request).then(function(response) {
-    const status = response.status;
-    alert(status);
-    if(status === 'OK'){
-      //
-    } else {
-      //
-    }
-
-    alert('abc');
-    return ['Android', 'NodeJS']
-  });
+  //var request = new Request('https://stg-id.gmo.jp/api/v1/member/get-uid', options);
+  Request
+    .post('https://stg-id.gmo.jp/api/v1/member/get-uid')
+    .withCredentials()
+    .set('Access-Control-Allow-Origin', '*')
+    .set('Access-Control-Allow-Credentials', 'true')
+    .set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    .set('Access-Control-Allow-Headers', 'gmo-id-point-api-token, Content-Type')
+    .set('gmo-id-point-api-token', token)
+    .set('Content-Type', 'application/json')
+    .send({ login: 'gmoidtest0001@gmail.com'})
+    .end((err, res) =>{
+      if (err || !res.ok) {
+        console.log(err);
+        alert('Oh no! error');
+      } else {
+        alert('yay got ' + JSON.stringify(res.body));
+      }
+    });
+  // fetch(request).then(function(response) {
+  //   console.log(response);
+  //   const status = response.status;
+  //   alert(status);
+  //   if(status === 'OK'){
+  //     //
+  //   } else {
+  //     //
+  //   }
+  //   return ['Android', 'NodeJS']
+  // });
 }
 // initialState
 const initialState = {
