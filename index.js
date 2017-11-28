d24f3f71b92f6361cecb059795aecfe03f7b43cf
phongnx <phongnx@runsystem.net>
2017-11-28 14:17:48 +0900

    change home file name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 app.set('views', './views');
-app.use(express.static('public'));
+app.use(express.static('./'));
 app.listen(3000, () => console.log('Server started'))
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/home.html');
+  res.sendFile(__dirname + '/index.html');
 });
